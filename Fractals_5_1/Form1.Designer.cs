b350199efff3ab7d10d4790b583da572dfd95a48
Nikita <nik_sikora@mail.com>
2021-10-22 16:21:59 +0300

    Добавьте файлы проекта.

---
+++ Fractals_5_1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Fractals_5_1
+{
+    partial class Fractals
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.ButtonDraw = new System.Windows.Forms.Button();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.buttonThickness = new System.Windows.Forms.Button();
+            this.checkBoxThickness = new System.Windows.Forms.CheckBox();
+            this.checkBoxColor = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(920, 780);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(1031, 34);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(348, 323);
+            this.textBoxDescription.TabIndex = 1;
+            // 
+            // ButtonDraw
+            // 
+            this.ButtonDraw.AutoSize = true;
+            this.ButtonDraw.Location = new System.Drawing.Point(1232, 401);
+            this.ButtonDraw.Name = "ButtonDraw";
+            this.ButtonDraw.Size = new System.Drawing.Size(147, 35);
+            this.ButtonDraw.TabIndex = 2;
+            this.ButtonDraw.Text = "Draw";
+            this.ButtonDraw.UseVisualStyleBackColor = true;
+            this.ButtonDraw.Click += new System.EventHandler(this.ButtonDraw_Click);
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(1062, 503);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(86, 31);
+            this.textBoxN.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1026, 509);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "n:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1232, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1031, 407);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(196, 29);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Random Angles";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(1031, 465);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(145, 29);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Auto Clear";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // buttonThickness
+            // 
+            this.buttonThickness.Location = new System.Drawing.Point(1026, 553);
+            this.buttonThickness.Name = "buttonThickness";
+            this.buttonThickness.Size = new System.Drawing.Size(201, 43);
+            this.buttonThickness.TabIndex = 8;
+            this.buttonThickness.Text = "Tree thickness";
+            this.buttonThickness.UseVisualStyleBackColor = true;
+            this.buttonThickness.Click += new System.EventHandler(this.buttonThickness_Click);
+            // 
+            // checkBoxThickness
+            // 
+            this.checkBoxThickness.AutoSize = true;
+            this.checkBoxThickness.Location = new System.Drawing.Point(1026, 623);
+            this.checkBoxThickness.Name = "checkBoxThickness";
+            this.checkBoxThickness.Size = new System.Drawing.Size(214, 29);
+            this.checkBoxThickness.TabIndex = 9;
+            this.checkBoxThickness.Text = "Thickness reduce";
+            this.checkBoxThickness.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxColor
+            // 
+            this.checkBoxColor.AutoSize = true;
+            this.checkBoxColor.Location = new System.Drawing.Point(1026, 677);
+            this.checkBoxColor.Name = "checkBoxColor";
+            this.checkBoxColor.Size = new System.Drawing.Size(193, 29);
+            this.checkBoxColor.TabIndex = 10;
+            this.checkBoxColor.Text = "Color Changing";
+            this.checkBoxColor.UseVisualStyleBackColor = true;
+            // 
+            // Fractals
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1448, 854);
+            this.Controls.Add(this.checkBoxColor);
+            this.Controls.Add(this.checkBoxThickness);
+            this.Controls.Add(this.buttonThickness);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.ButtonDraw);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Fractals";
+            this.Text = "Fractals";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Button ButtonDraw;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button buttonThickness;
+        private System.Windows.Forms.CheckBox checkBoxThickness;
+        private System.Windows.Forms.CheckBox checkBoxColor;
+    }
+}
+
---
+++ Fractals_5_1/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Fractals_5_1
+{
+    public partial class Fractals : Form
+    {
+        public class State
+        {
+            public Point point1;
+            public Point point2;
+            public int degree;
+            public Pen pen;
+
+            public State(Point point1,Point point2,int degree, Pen pen)
+            {
+                this.point1 = point1;
+                this.point2 = point2;
+                this.degree = degree;
+                this.pen = pen;
+            }
+        }
+
+        public Fractals()
+        {
+            InitializeComponent();
+            start_position.X = -1;
+            textBoxN.Text = "1";
+        }
+
+        string atom;
+        int angle_degree;
+        string direction;
+        string[] rules;
+        int n;
+        Point start_position;
+        Stack<State> points = new Stack<State>();
+        int LineLength = 25;
+        int degree_change = 0;
+        static Bitmap bmp = new Bitmap(920, 780);
+        public Graphics g = Graphics.FromImage(bmp);
+
+        void Setup()
+        {
+            rules = Regex.Split(textBoxDescription.Text, "\r\n|\r|\n");
+            string[] config = rules[0].Split();
+            atom = config[0];
+            angle_degree = int.Parse(config[1]);
+            direction = config[2];
+            n = int.Parse(textBoxN.Text);
+            pictureBox1.Image = bmp;
+            
+        }
+
+        void ComputeResultString()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (var pos = 0; pos < atom.Length; pos++)
+                {
+                    for (int rule_num = 1; rule_num < rules.Length; rule_num++)
+                    {
+                        var curr_rule = rules[rule_num].Split('>');
+                        if (atom[pos].ToString() == curr_rule[0])
+                        {
+                            atom = atom.Remove(pos, 1).Insert(pos, curr_rule[1]);
+                            pos += curr_rule[1].Length - 1;
+                            break;
+                        }
+                    }
+                }
+
+                //for (int j = 1; j < rules.Length; j++)
+                //{
+                //    var curr_rule = rules[j].Split('>');
+                //    atom = atom.Replace(curr_rule[0], curr_rule[1]);
+                //}
+                //MessageBox.Show(atom);
+            }
+
+            for (int j = 2; j < rules.Length; j++)
+            {
+                var curr_rule = rules[j].Split('>');
+                atom = atom.Replace(curr_rule[0], "");
+            }
+            //MessageBox.Show(atom);
+        }
+
+        void Fill_Points()
+        {
+            
+            degree_change = 0;
+            //points.Push(start_position);
+            double x1 = start_position.X;
+            double y1 = start_position.Y;
+            double x2 = start_position.X;
+            double y2 = start_position.Y;
+            if (direction == "right")
+                x2 += LineLength;
+            else
+                if (direction == "left")
+                x2 -= LineLength;
+            else
+                if (direction == "up")
+                y2 += LineLength;
+            else
+                if (direction == "down")
+                y2 -= LineLength;
+            else
+            {
+                MessageBox.Show("Invalid direction!");
+                //Fill_Points();
+                return;
+            }
+
+            //MessageBox.Show(points.Peek().X + " " + points.Peek().Y);
+            double x1_reserve = x1;
+            double y1_reserve = y1;
+
+            double x2_reserve = x2;
+            double y2_reserve = y2;
+
+            Stack<State> states = new Stack<State>();
+            int degree_change_reserve = degree_change;
+            
+
+            Random rnd = new Random();
+
+            Pen pen;
+            Color color = Color.Black;
+            if (checkBoxColor.Checked)
+                color = Color.Brown;
+            if (!checkBoxThickness.Checked)
+                pen = new Pen(color, 1f);
+            else
+                pen = new Pen(color, 10f);
+            Pen pen_reserve = pen;
+
+            foreach (var atm in atom)
+            {
+                
+                switch (atm)
+                {
+                    case 'F':
+                        double angle = degree_change * Math.PI / 180.0;
+                        var xx = x2;
+                        x2 = (x1 + (x2 - x1) * Math.Cos(angle) - (y2 - y1) * Math.Sin(angle));
+                        y2 = (y1 + (xx - x1) * Math.Sin(angle) + (y2 - y1) * Math.Cos(angle));
+                        y2 = y1 + y1 - y2;
+
+                        points.Push(new State(new Point((int)x1, (int)y1), new Point((int)x2, (int)y2), degree_change,(Pen)pen.Clone()));
+                        pen.Width--;
+                        if (checkBoxColor.Checked)
+                        {
+                            color = pen.Color;
+                            color = Color.FromArgb(Math.Max(color.R - 20, 0), Math.Min(color.G + 20, 255), Math.Max(color.B - 20, 0));
+                            pen.Color = color;
+                        }
+                        //MessageBox.Show(points.Peek().X + " "+ points.Peek().Y);
+                        x1 = x2;
+                        y1 = y2;
+                        if (direction == "right")
+                            x2 += LineLength;
+                        else
+                            if (direction == "left")
+                            x2 -= LineLength;
+                        else
+                            if (direction == "up")
+                            y2 += LineLength;
+                        else
+                            if (direction == "down")
+                            y2 -= LineLength;
+
+                        if (x2 >= pictureBox1.Width - 5 || y2 >= pictureBox1.Height - 5 || x2 <= 5 || y2 <= 5)
+                        {
+                            //while (x2 > pictureBox1.Width || y2 > pictureBox1.Height || x2 < 0 || y2 < 0)
+                            LineLength -= 1;
+                            //MessageBox.Show(LineLength + "");
+                            if (checkBox2.Checked)
+                            {
+                                g.Clear(Color.White);
+                                pictureBox1.Image = bmp;
+                            }
+                            Setup();
+                            ComputeResultString();
+                            while (points.Count() > 0)
+                                points.Pop();
+                            Fill_Points();
+
+                            return;
+                        }
+                        break;
+                    case '+':
+                        if (!checkBox1.Checked)
+                            degree_change += angle_degree;
+                        else
+                        {
+                            int num = rnd.Next(angle_degree/2,angle_degree+angle_degree/2);
+                            degree_change += num;
+                        }
+                        break;
+                    case '-':
+                        if (!checkBox1.Checked)
+                            degree_change -= angle_degree;
+                        else
+                        {
+                            int num = rnd.Next(angle_degree/2, angle_degree + angle_degree/2);
+                            degree_change -= num;
+                        }
+                        break;
+                    case '[':
+
+                        x1_reserve = x1;
+                        y1_reserve = y1;
+
+                        x2_reserve = x2;
+                        y2_reserve = y2;
+
+                        degree_change_reserve = degree_change;
+
+                        pen_reserve = pen;
+
+                        states.Push(new State(new Point((int)x1_reserve, (int)y1_reserve), new Point((int)x2_reserve, (int)y2_reserve), degree_change_reserve, (Pen)pen_reserve.Clone()));
+                        break;
+                    case ']':
+                        State curr_line = states.Pop();
+                        x1 = curr_line.point1.X;
+                        y1 = curr_line.point1.Y;
+
+                        x2 = curr_line.point2.X;
+                        y2 = curr_line.point2.Y;
+
+                        degree_change = curr_line.degree;
+                        pen = curr_line.pen;
+                        break;
+                }
+            }
+        }
+
+        void Draw_Points()
+        {
+            while (points.Count > 0)
+            {
+                State line = points.Pop();
+                g.DrawLine(line.pen, line.point1.X, line.point1.Y, line.point2.X, line.point2.Y) ;
+                //Line_Bres(first.X, first.Y, second.X, second.Y);
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        void Line_Bres(int x0, int y0, int x1, int y1)
+        {
+            int xtemp0 = x0;
+            int xtemp1 = x1;
+            int ytemp0 = y0;
+            int ytemp1 = y1;
+            if (Math.Abs(x1 - x0) < Math.Abs(y1 - y0))
+            {
+                Swap(ref x0, ref y0);
+                Swap(ref x1, ref y1);
+            }
+            if (x0 > x1)
+            {
+                Swap(ref x0, ref x1);
+                Swap(ref y0, ref y1);
+            }
+            int deltax = Math.Abs(x1 - x0);
+            int deltay = Math.Abs(y1 - y0);
+            int error = 0;
+            int deltaerr = (deltay + 1);
+            int y = y0;
+            int diry = y1 - y0;
+            if (diry > 0)
+                diry = 1;
+            if (diry < 0)
+                diry = -1;
+            for (int x = x0; x <= x1; x++)
+            {
+                bmp.SetPixel(Math.Abs(ytemp1 - ytemp0) > Math.Abs(xtemp1 - xtemp0) ? y : x, Math.Abs(ytemp1 - ytemp0) > Math.Abs(xtemp1 - xtemp0) ? x : y, Color.Black);
+                error = error + deltaerr;
+                if (error >= deltax + 1)
+                {
+                    y += diry;
+                    error = error - (deltax + 1);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+        static void Swap(ref int x, ref int y)
+        {
+            int t = x;
+            x = y;
+            y = t;
+        }
+        private void ButtonDraw_Click(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                g.Clear(Color.White);
+                pictureBox1.Image = bmp;
+            }
+            
+            Setup();
+            if(start_position.X != -1)
+            {
+                ComputeResultString();
+                LineLength = 25;
+                Fill_Points();
+                Draw_Points();
+            }
+            else
+                MessageBox.Show("Определите начальное положение");
+            
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            start_position.X = e.X;
+            start_position.Y = e.Y;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Image = bmp;
+        }
+
+        private void buttonThickness_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
